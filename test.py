d41273506f7ce5778f51ab77cfe989cb69e4f951
AbyssSkb <abyssskb@outlook.com>
2023-10-04 13:57:38 +0800

    modify test.py

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import torch
 from My_network import Unet
-from My_dataset import My_Dataset2
+from My_dataset import testDataset
 from torch.utils.data import DataLoader
 import torchaudio
 import os
 
-test_loader = DataLoader(dataset=My_Dataset2())
+test_loader = DataLoader(dataset=testDataset(), shuffle=True)
 
 def test():
     model = Unet(in_channel=1, out_channel=2)
@@ -14,13 +14,15 @@ def test():
 
     i = 1
     for data in test_loader:
+        if i > 50:
+            break
         audio_merge, _ = data
         audio_merge = audio_merge.cuda()
         output = model(audio_merge).cpu().detach()
         audio_merge = audio_merge.cpu().detach()
-        torchaudio.save(os.path.join('C:\\Users\\Abyss\\Music\Test', 'spilt-' + str(i) + '_1.wav'), output[:, 0, :], 16000)
-        torchaudio.save(os.path.join('C:\\Users\\Abyss\\Music\Test', 'spilt-' + str(i) + '_2.wav'), output[:, 1, :], 16000)
-        torchaudio.save(os.path.join('C:\\Users\\Abyss\\Music\Test', 'merge-' + str(i) + '.wav'), audio_merge[0, :, :], 16000)
+        torchaudio.save(os.path.join('C:\\Users\\Abyss\\Music\Test', str(i) + '_1.wav'), output[:, 0, :], 16000)
+        torchaudio.save(os.path.join('C:\\Users\\Abyss\\Music\Test', str(i) + '_2.wav'), output[:, 1, :], 16000)
+        torchaudio.save(os.path.join('C:\\Users\\Abyss\\Music\Test', str(i) + '_0.wav'), audio_merge[0, :, :], 16000)
         i += 1
 
 if __name__ == '__main__':
