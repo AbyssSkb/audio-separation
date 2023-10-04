88402c0a16a15c2f2f2e45b88c0ae078c096519f
AbyssSkb <abyssskb@outlook.com>
2023-10-04 15:37:07 +0800

    version 4

--- train.py
+++ train.py
@@ -6,13 +6,13 @@ import torch
 
 Epoch = 100
 Batch_size = 5
-LR = 0.0003
+LR = 0.0001
 
 train_loader = DataLoader(dataset=trainDataset(), batch_size=Batch_size, shuffle=True)
 
 def main():
     model = Unet(in_channel=1, out_channel=2)
-    model.load_state_dict(torch.load("model2.pt"))
+    model.load_state_dict(torch.load("model.pt"))
     loss_function = nn.MSELoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=LR)
     model = model.cuda()
@@ -30,7 +30,7 @@ def main():
             optimizer.step()
             print('Train Loss: %.1e' %loss.item())
 
-        torch.save(model.state_dict(), "model2.pt")
+        torch.save(model.state_dict(), "model.pt")
 
 if __name__ == '__main__':
     main()
